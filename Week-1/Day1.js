e3fda39d9b2dfd5f9eb2f2244f1f26859c347981
triptibhatnagar <bhatnagartripti2727@gmail.com>
2024-03-17 20:24:18 +0530

    Modified

--- Week-1/Day1.js
+++ Week-1/Day1.js
@@ -1,23 +1,27 @@
 // VARIABLES - containers used to store data
-// "use strict" -> treat all JS code acc to newer JS standards
-// var - can be changed, redeclared
+// "use strict" -> // treat all JS code acc to newer JS standards
+// var - can be reassigned & redeclared & left undefined, global scope
 var fullName = "Virat Kohli"
 var fullName = "KL Rahul"
 fullName = "Abhishek Malhan"
-console.log(fullName) //Abhishek
+console.log(fullName) //Abhishek Malhan
 var a;
 console.log(a)
 
-//ES-6 features -let,const
-//let - can be changed, but not redeclared
+//ES-6 features - let,const
+//let - can be reassigned & left undefined, but not redeclared, block scope
 let lang = "JavaScript"
 lang = "Python"
 console.log(lang)
 let b;
 console.log(b)
 
-//const - can't be changed, redeclared or left undefined
-const PI = 3.142;
+//const - can't be changed, redeclared or left undefined, block scope
+{const PI = 3.142;
+console.log(PI)
+console.log(fullName)}
+
+const PI = 314*0.01;
 console.log(PI)
 // const e; //SyntaxError - Missing initialiser
 
@@ -28,8 +32,9 @@ state = "Ambala" //It implies that JS is dynamically typed lang
 console.log(state)
 
 
-// DATATYPES - primitive and non-primitive
-// Primitive - 7 types 
+// DATATYPES - refers to the type of value a variable
+// primitive and non-primitive
+// Primitive - 7 types
 let district = "Haryana" //string
 console.log(district,typeof(district))
 
@@ -39,7 +44,7 @@ console.log(income,typeof(income))
 let isLoggedIn = false //boolean
 console.log(isLoggedIn,typeof(isLoggedIn))
 
-let score = BigInt("122111") //bigint
+let score = BigInt("122111") //bigint - used when integer values are larger than the range supported by the 'number' data type
 console.log(score,typeof score)
 
 let input;//undefined
@@ -48,5 +53,11 @@ console.log(input,typeof(input))
 let output = null;//null -> empty
 console.log(output,typeof(output))
 
-let uniqueVal = Symbol('a');//symbol -> expressing uniqueness
+/*
+differnce b/w null and undefined :-
+undefined means a variable has been declared but has not yet been assigned a value
+null is an assignment value, meaning that a variable has been declared and given the value of null
+*/
+
+let uniqueVal = Symbol('a');//symbol -> used to make a variable unique
 console.log(uniqueVal,typeof uniqueVal)
