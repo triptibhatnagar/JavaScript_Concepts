3d932746535182bca661f21617ced67aaa7da36d
triptibhatnagar <bhatnagartripti2727@gmail.com>
2024-04-01 21:46:27 +0530

    expOfCode

--- Week-3/project1.js
+++ Week-3/project1.js
@@ -1,13 +1,18 @@
+// select all buttons
 let allBtns = document.querySelectorAll('.button')
+// select the body
 const body = document.querySelector('body')
 
+// allBtns is a nodeList, so we apply forEach to access each button
 allBtns.forEach((btn) => {
     console.log(btn);
+    // event listener added for 'click' event and a callback function is passed to execute some work on mouseclick
     btn.addEventListener('click', (event) => {
         console.log(event);
-        console.log(event.target);
+        console.log(event.target);// tells about who occured the event
+        // if statements
         if (event.target.id === 'deeppink') {
-            body.style.backgroundColor = event.target.id;
+            body.style.backgroundColor = event.target.id;//bg-color of body will be changed on mouseclick to a button
           }
         if (event.target.id === 'white') {
             body.style.backgroundColor = event.target.id;
